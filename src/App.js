f0d6eaa8d24ac715b92a3ae07007cd8c453e7bf1
David Guevara <david.guevara@external.appspace.com>
2021-05-20 17:08:31 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Templates } from './toolkit';
+
+import templateData from './config/template';
+import sectionsData from './config/sections';
+import inputsData from './config/inputs';
+import threekitConfig from './config/threekitConfig';
+
+const App = () => {
+  return (
+    <Templates
+      template={templateData}
+      sections={sectionsData}
+      inputs={inputsData}
+      threekitConfig={threekitConfig}
+    />
+  );
+};
+
+export default App;
